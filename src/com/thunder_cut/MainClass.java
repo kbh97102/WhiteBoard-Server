6be168128d9ec0ea432067b445dc381f519b0757
Seokjin Yoon <sjy5386@gmail.com>
2020-01-10 18:40:24 +0900

    Add MainClass

---
+++ src/com/thunder_cut/MainClass.java
@@ -0,0 +1,13 @@
+/*
+ * MainClass.java
+ * Author: Seokjin Yoon
+ * Created Date: 2020-01-10
+ */
+
+package com.thunder_cut;
+
+public class MainClass {
+    public static void main(String[] args) {
+
+    }
+}
