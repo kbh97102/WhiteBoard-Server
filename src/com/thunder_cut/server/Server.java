ad850fbb77eb600462629b435838bac0ac217457
Seokjin Yoon <sjy5386@gmail.com>
2020-01-17 20:13:05 +0900

    Remove duplicated codes

--- src/com/thunder_cut/server/Server.java
+++ src/com/thunder_cut/server/Server.java
@@ -23,61 +23,46 @@ import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 import java.util.Vector;
 
-public class Server implements Runnable{
-
-    private final static int PORT = 3001;
+public class Server implements Runnable {
+    private static final int PORT = 3001;
     private AsynchronousServerSocketChannel serverSocket;
     private Vector<Attachment> clientGroup = new Vector<>();
-    private Charset charset = StandardCharsets.UTF_8;
+    private static final Charset charset = StandardCharsets.UTF_8;
     private ByteBuffer buffer;
 
     /**
      * All IP, Default Port
      */
     public Server() {
-        try {
-            serverSocket = AsynchronousServerSocketChannel.open();
-            serverSocket.bind(new InetSocketAddress(PORT));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        this(null, PORT);
     }
 
     /**
      * All IP, Custom Port
+     *
      * @param port Custom Port is that user want to connect
      */
     public Server(int port) {
-        try {
-            serverSocket = AsynchronousServerSocketChannel.open();
-            serverSocket.bind(new InetSocketAddress(port));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        this(null, port);
     }
 
     /**
      * Specific IP, Default Port
+     *
      * @param ip Specific IP is that user want to connect
      */
     public Server(String ip) {
-        try {
-            serverSocket = AsynchronousServerSocketChannel.open();
-            serverSocket.bind(new InetSocketAddress(ip, PORT));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        this(ip, PORT);
     }
 
     /**
-     *
-     * @param ip Custom IP
+     * @param ip   Custom IP
      * @param port Custom Port
      */
     public Server(String ip, int port) {
         try {
             serverSocket = AsynchronousServerSocketChannel.open();
-            serverSocket.bind(new InetSocketAddress(ip, port));
+            serverSocket.bind(ip == null ? new InetSocketAddress(port) : new InetSocketAddress(ip, port));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -87,7 +72,7 @@ public class Server implements Runnable{
      * When this method is called Server Will be waiting for connect from client with ServerAcceptHandler
      * and Start reading with ServerReadHandler
      */
-    public void startAccept(){
+    public void startAccept() {
         Attachment ServerInfo = new Attachment();
         ServerInfo.setServer(serverSocket);
         ServerInfo.setClientGroup(clientGroup);
@@ -97,45 +82,47 @@ public class Server implements Runnable{
     /**
      * If Client is not readMode start Reading
      */
-    public void readFromClient(){
-        for(Attachment clientInfo : clientGroup){
-            if(!clientInfo.isReadMode()){
+    public void readFromClient() {
+        for (Attachment clientInfo : clientGroup) {
+            if (!clientInfo.isReadMode()) {
                 clientInfo.setReadMode(true);
-                clientInfo.getClient().read(clientInfo.getBuffer(),clientInfo, new ServerReadHandler().getReadHandler());
+                clientInfo.getClient().read(clientInfo.getBuffer(), clientInfo, new ServerReadHandler().getReadHandler());
             }
         }
     }
 
     /**
      * Send String Message to All client in ClientGroup
+     *
      * @param message String Message
      */
-    public void writeToAllClients(String message){
+    public void writeToAllClients(String message) {
         buffer = charset.encode(message);
-        for(Attachment clientInfo : clientGroup){
-            clientInfo.getClient().write(buffer, buffer,new ServerWriteHandler().getWriteHandler());
+        for (Attachment clientInfo : clientGroup) {
+            clientInfo.getClient().write(buffer, buffer, new ServerWriteHandler().getWriteHandler());
         }
         buffer.clear();
     }
 
     /**
      * Convert Image Data to ByteBuffer and Send all client
+     *
      * @param imageLabel JLabel have a image for sending
      */
-    public void writeToAllClients(JLabel imageLabel){
+    public void writeToAllClients(JLabel imageLabel) {
         try {
             ImageIcon imageIcon = (ImageIcon) imageLabel.getIcon();
             Image image = imageIcon.getImage();
-            BufferedImage bufferedImage = new BufferedImage(image.getWidth(null),image.getHeight(null),BufferedImage.TYPE_INT_RGB);
+            BufferedImage bufferedImage = new BufferedImage(image.getWidth(null), image.getHeight(null), BufferedImage.TYPE_INT_RGB);
             Graphics2D graphics = bufferedImage.createGraphics();
-            graphics.drawImage(image,null,null);
+            graphics.drawImage(image, null, null);
             graphics.dispose();
             ByteArrayOutputStream output = new ByteArrayOutputStream();
             ImageIO.write(bufferedImage, "png", output);
             output.flush();
             ByteBuffer buffer = ByteBuffer.wrap(output.toByteArray());
 
-            for(Attachment client : clientGroup){
+            for (Attachment client : clientGroup) {
                 client.getClient().write(buffer);
             }
         } catch (IOException e) {
@@ -145,10 +132,11 @@ public class Server implements Runnable{
 
     /**
      * Pick specific client and send String message
-     * @param message String message
+     *
+     * @param message     String message
      * @param clientIndex Sending Target
      */
-    public void writeToSpecificClient(String message, int clientIndex){
+    public void writeToSpecificClient(String message, int clientIndex) {
         buffer = charset.encode(message);
         clientGroup.get(clientIndex).getClient().write(buffer, buffer, new ServerWriteHandler().getWriteHandler());
         buffer.clear();
@@ -156,16 +144,17 @@ public class Server implements Runnable{
 
     /**
      * Convert Image Data to ByteBuffer and Send specific client
-     * @param imageLabel JLabel have a image for sending
+     *
+     * @param imageLabel  JLabel have a image for sending
      * @param clientIndex Receiving target
      */
-    public void writeToSpecificClient(JLabel imageLabel, int clientIndex){
+    public void writeToSpecificClient(JLabel imageLabel, int clientIndex) {
         try {
             ImageIcon imageIcon = (ImageIcon) imageLabel.getIcon();
             Image image = imageIcon.getImage();
-            BufferedImage bufferedImage = new BufferedImage(image.getWidth(null),image.getHeight(null),BufferedImage.TYPE_INT_RGB);
+            BufferedImage bufferedImage = new BufferedImage(image.getWidth(null), image.getHeight(null), BufferedImage.TYPE_INT_RGB);
             Graphics2D graphics = bufferedImage.createGraphics();
-            graphics.drawImage(image,null,null);
+            graphics.drawImage(image, null, null);
             graphics.dispose();
             ByteArrayOutputStream output = new ByteArrayOutputStream();
             ImageIO.write(bufferedImage, "png", output);
@@ -180,7 +169,7 @@ public class Server implements Runnable{
 
     /**
      * If you use GUI remove Implement Runnable
-     *
+     * <p>
      * If user input Quit writing will shutdown
      */
     @Override
@@ -189,7 +178,7 @@ public class Server implements Runnable{
 
         String input;
         Scanner scanner = new Scanner(System.in);
-        while(!(input = scanner.nextLine()).equals("Quit")){
+        while (!(input = scanner.nextLine()).equals("Quit")) {
             writeToAllClients(input);
         }
 
@@ -197,8 +186,7 @@ public class Server implements Runnable{
             Thread.currentThread().join();
         } catch (InterruptedException e) {
             e.printStackTrace();
-        }
-        finally {
+        } finally {
             try {
                 serverSocket.close();
                 scanner.close();
