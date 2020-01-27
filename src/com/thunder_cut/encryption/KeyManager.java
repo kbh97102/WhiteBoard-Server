391e874a5282c1328b83d33b5477b2b26fdc55a3
Seokjin Yoon <sjy5386@gmail.com>
2020-01-27 19:16:59 +0900

    Add KeyManager

---
+++ src/com/thunder_cut/encryption/KeyManager.java
@@ -0,0 +1,36 @@
+/*
+ * KeyManager.java
+ * Author: Seokjin Yoon
+ * Created Date: 2020-01-27
+ */
+
+package com.thunder_cut.encryption;
+
+import com.thunder_cut.socket.Attachment;
+
+import java.security.PublicKey;
+import java.util.HashMap;
+
+public class KeyManager {
+    private static String symmetricKey = null;
+    private static HashMap<Attachment, PublicKey> publicKey = new HashMap<>();
+
+    private KeyManager() {
+    }
+
+    public static String getSymmetricKey() {
+        return symmetricKey;
+    }
+
+    public static void setSymmetricKey(String key) {
+        symmetricKey = key;
+    }
+
+    public static PublicKey getPublicKey(Attachment client) {
+        return publicKey.get(client);
+    }
+
+    public static void setPublicKey(Attachment client, PublicKey key) {
+        publicKey.put(client, key);
+    }
+}
