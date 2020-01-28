4772b5593243f9c3d9caed54199f6dba1f1ee4de
Seokjin Yoon <sjy5386@gmail.com>
2020-01-28 12:39:14 +0900

    Add PublicKeyEncryption

---
+++ src/com/thunder_cut/encryption/PublicKeyEncryption.java
@@ -0,0 +1,34 @@
+/*
+ * PublicKeyEncryption.java
+ * Author: Seokjin Yoon
+ * Created Date: 2020-01-28
+ */
+
+package com.thunder_cut.encryption;
+
+import javax.crypto.Cipher;
+import java.security.Key;
+import java.security.PublicKey;
+import java.util.Base64;
+
+public class PublicKeyEncryption {
+    private PublicKeyEncryption() {
+    }
+
+    public static byte[] encrypt(byte[] data, PublicKey key) {
+        try {
+            byte[] encrypted = getCipher(Cipher.ENCRYPT_MODE, key).doFinal(data);
+            byte[] encoded = Base64.getEncoder().encode(encrypted);
+            return encoded;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private static Cipher getCipher(int mode, Key key) throws Exception {
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(mode, key);
+        return cipher;
+    }
+}
