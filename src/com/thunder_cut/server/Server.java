48c7e0d93fdb0e75173252d23c4a056128bb0619
kbh97102 <50691301+kbh97102@users.noreply.github.com>
2020-02-03 14:17:39 +0900

    Add Server Class Constructor

---
+++ src/com/thunder_cut/server/Server.java
@@ -0,0 +1,52 @@
+package com.thunder_cut.server;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.AsynchronousServerSocketChannel;
+import java.util.Vector;
+
+public class Server {
+    private static final int PORT = 3001;
+
+    private AsynchronousServerSocketChannel server;
+    private Vector<ClientAttachment> clientGroup = new Vector<>();
+
+    /**
+     * All IP, Default Port
+     */
+    public Server() {
+        this(null, PORT);
+    }
+
+    /**
+     * All IP, Custom Port
+     *
+     * @param port Custom Port is that user want to connect
+     */
+    public Server(int port) {
+        this(null, port);
+    }
+
+    /**
+     * Specific IP, Default Port
+     *
+     * @param ip Specific IP is that user want to connect
+     */
+    public Server(String ip) {
+        this(ip, PORT);
+    }
+
+    /**
+     * @param ip   Custom IP
+     * @param port Custom Port
+     */
+    public Server(String ip, int port) {
+        try {
+            server = AsynchronousServerSocketChannel.open();
+            server.bind(ip == null ? new InetSocketAddress(port) : new InetSocketAddress(ip, port));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
