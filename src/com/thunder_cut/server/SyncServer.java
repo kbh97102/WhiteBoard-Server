89d4c309910e1b2eca91f191f891913b28af9240
kbh97102 <50691301+kbh97102@users.noreply.github.com>
2020-02-17 18:53:38 +0900

    Update SyncServer Class

--- src/com/thunder_cut/server/SyncServer.java
+++ src/com/thunder_cut/server/SyncServer.java
@@ -9,8 +9,8 @@ package com.thunder_cut.server;
 import com.thunder_cut.server.data.DataType;
 
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
-import java.nio.ByteBuffer;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.util.*;
@@ -20,32 +20,24 @@ import java.util.concurrent.Executors;
 /**
  * This class is main class about server
  * If program start, generate server and bind given data
- * Accept method keep running until program is shutdown
  */
 public class SyncServer {
 
-    private static final Map<Character, DataType> dataTypeMap;
     private static final int PORT = 3001;
     private ServerSocketChannel server;
     private ExecutorService executorService;
     private final List<ClientInfo> clientGroup;
-
-    static {
-        dataTypeMap = new HashMap<>();
-        for (DataType dataTypeEnum : DataType.values()) {
-            dataTypeMap.put(dataTypeEnum.type, dataTypeEnum);
-        }
-    }
+    private final Map<ClientInfo, List<ClientInfo>> blindMap;
 
     /**
-     * All IP, Default Port
+     * Connect with All IP, Default Port
      */
     public SyncServer() {
         this(null, PORT);
     }
 
     /**
-     * All IP, Custom Port
+     * Connect with All IP, Custom Port
      *
      * @param port Custom Port is that user want to connect
      */
@@ -54,7 +46,7 @@ public class SyncServer {
     }
 
     /**
-     * Specific IP, Default Port
+     * Connect with Specific IP, Default Port
      *
      * @param ip Specific IP is that user want to connect
      */
@@ -63,6 +55,8 @@ public class SyncServer {
     }
 
     /**
+     * Connect with Custom IP, Custom Port
+     *
      * @param ip   Custom IP
      * @param port Custom Port
      */
@@ -70,15 +64,22 @@ public class SyncServer {
         try {
             server = ServerSocketChannel.open();
             server.bind(ip == null ? new InetSocketAddress(port) : new InetSocketAddress(ip, port));
+            InetAddress local = InetAddress.getLocalHost();
+            System.out.println(local.getHostAddress());
         } catch (IOException e) {
             e.printStackTrace();
         }
         initialize();
         clientGroup = Collections.synchronizedList(new ArrayList<>());
+        blindMap = new HashMap<>();
     }
 
     private void initialize() {
         executorService = Executors.newFixedThreadPool(10);
+    }
+
+    private synchronized Map<ClientInfo, List<ClientInfo>> getMap() {
+        return blindMap;
     }
 
     /**
@@ -89,20 +90,29 @@ public class SyncServer {
     }
 
     /**
-     * Detect client connection and Generate ClientInformation with connected client
-     * After Generating Add clientGroup and start readingData from client
+     * Detect client connection and Generate ClientInfo with connected client
+     * After Generating,  Add clientGroup and start reading Data from client
      */
     private void accept() {
-        ClientInfo.WriteCallBack sending = this::identifyWriteMode;
+        ClientInfo.DisconnectCallBack disconnectCallBack = this::disconnect;
         while (true) {
             try {
                 SocketChannel client = server.accept();
                 System.out.println(client.getRemoteAddress() + " is connect");
                 ClientInfo clientInformation = new ClientInfo(clientGroup.size());
                 clientInformation.setClient(client);
-                clientInformation.setSending(sending);
+                clientInformation.setCallBack(disconnectCallBack, this::getMap);
+                clientInformation.setOp(false);
                 synchronized (clientGroup) {
                     clientGroup.add(clientInformation);
+                }
+                synchronized (blindMap) {
+                    for (ClientInfo information : blindMap.keySet()) {
+                        blindMap.get(information).add(clientInformation);
+                    }
+                    List<ClientInfo> list = Collections.synchronizedList(new ArrayList<>());
+                    list.addAll(clientGroup);
+                    blindMap.put(clientInformation, list);
                 }
                 clientInformation.read();
             } catch (IOException e) {
@@ -112,77 +122,46 @@ public class SyncServer {
     }
 
     /**
-     * Check dataType and decide writeMode
-     * If type is command, send data to srcID
-     * or write to everyone
-     * @param srcID client who send data
-     * @param type data type
-     * @param buffer pure data(No header)
-     */
-    private void identifyWriteMode(int srcID, char type, ByteBuffer buffer) {
-        if (dataTypeMap.get(type) == DataType.CMD) {
-            writeToSrc(srcID, type, buffer);
-        } else {
-            writeToAll(srcID, type, buffer);
-        }
-    }
-
-    /**
-     * Generate with srcID, data type, pure data and Write to everyone in clientGroup
-     * @param srcID client who send data
-     * @param type data type
-     * @param buffer pure data(No header)
-     */
-    private void writeToAll(int srcID, char type, ByteBuffer buffer) {
-        synchronized (clientGroup) {
-            for (ClientInfo destination : clientGroup) {
-                SendingData sendingData = new SendingData(srcID, destination.ID, dataTypeMap.get(type), buffer.array());
-                try {
-                    destination.getClient().write(sendingData.toByteBuffer());
-                } catch (IOException e) {
-                    removeClient(destination);
-                    return;
-                }
-            }
-        }
-    }
-
-    /**
-     * Generate with srcID, data type, pure data and Write to given srcID
-     * @param srcID client who send data
-     * @param type data type
-     * @param buffer pure data(No header)
-     */
-    private void writeToSrc(int srcID, char type, ByteBuffer buffer) {
-        synchronized (clientGroup) {
-            for (ClientInfo destination : clientGroup) {
-                if (destination.ID == srcID) {
-                    SendingData sendingData = new SendingData(srcID, destination.ID, dataTypeMap.get(type), buffer.array());
-                    try {
-                        destination.getClient().write(sendingData.toByteBuffer());
-                    } catch (IOException e) {
-                        removeClient(destination);
-                        return;
-                    }
-                    break;
-                }
-            }
-        }
-    }
-
-    /**
      * Remove disconnected client in clientGroup
      * After that change client's ID by ascending sort
+     *
      * @param removeTarget disconnected client
      */
-    private void removeClient(ClientInfo removeTarget) {
+    private void disconnect(ClientInfo removeTarget) {
         System.out.println("Client " + removeTarget.ID + " is disconnected");
-        clientGroup.remove(removeTarget);
         synchronized (clientGroup) {
+            clientGroup.remove(removeTarget);
             for (int i = 0; i < clientGroup.size(); i++) {
                 clientGroup.get(i).ID = i;
             }
         }
+        synchronized (blindMap) {
+            blindMap.remove(removeTarget);
+            for (ClientInfo information : blindMap.keySet()) {
+                blindMap.get(information).remove(removeTarget);
+                for (int i = 0; i < blindMap.get(information).size(); i++) {
+                    blindMap.get(information).get(i).ID = i;
+                }
+            }
+        }
+        try {
+            removeTarget.getClient().close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
+    private void connectionClear(){
+        for(ClientInfo key : blindMap.keySet()){
+            for(ClientInfo client : blindMap.get(key)){
+                try {
+                    client.getClient().close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            blindMap.remove(key);
+        }
+        clientGroup.clear();
+    }
 }
