7ea36f63bdb6b959ca2018502d517e4d9471be12
kbh97102 <50691301+kbh97102@users.noreply.github.com>
2020-02-17 18:52:23 +0900

    Update ClientInfo Class

--- src/com/thunder_cut/server/ClientInfo.java
+++ src/com/thunder_cut/server/ClientInfo.java
@@ -1,27 +1,50 @@
 /*
- * ClientInformation.java
+ * ClientInfo.java
  * Author : Arakene
  * Created Date : 2020-02-04
  */
 package com.thunder_cut.server;
 
+import com.thunder_cut.server.data.DataType;
+import com.thunder_cut.server.data.ReceivedData;
+
 import java.io.IOException;
+import java.nio.BufferUnderflowException;
 import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.nio.channels.SocketChannel;
+import java.util.List;
+import java.util.Map;
 
 /**
  * This class has information, read and write about connected client
  */
 public class ClientInfo {
 
-    interface WriteCallBack {
-        void write(int srcID, char type, ByteBuffer buffer);
+    interface DisconnectCallBack {
+        void disconnect(ClientInfo client);
+    }
+
+    interface CallClientMap {
+        Map<ClientInfo, List<ClientInfo>> getMap();
     }
 
     public int ID;
-
+    private String name;
     private SocketChannel client;
-    private WriteCallBack writeToAll;
+    private DisconnectCallBack disconnectCallBack;
+    private Process processing;
+    private boolean op;
+
+    public boolean isOp() {
+        return op;
+    }
+
+    public void setOp(boolean op) {
+        this.op = op;
+    }
+
+    private CallClientMap callMap;
 
     public ClientInfo(int ID) {
         this.ID = ID;
@@ -39,19 +62,34 @@ public class ClientInfo {
         new Thread(this::reading).start();
     }
 
-    public void setSending(WriteCallBack callBack) {
-        writeToAll = callBack;
+    public void setCallBack(DisconnectCallBack disconnect, CallClientMap callMap) {
+        this.disconnectCallBack = disconnect;
+        this.callMap = callMap;
+
+        processing = new Process(disconnect::disconnect);
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
     }
 
     /**
-     *  1. Allocate ByteBuffer with header size and read header
-     *  2. Figure out data type, data size and allocate ByteBuffer with data size
-     *  3. Receive data until buffer has no space
-     *  4. Give ID, data type and data(no header) to write
+     * 1. Allocate ByteBuffer with header size and read header
+     * <p>
+     * 2. Figure out data type, data size and allocate ByteBuffer with data size
+     * <p>
+     * 3. Receive data until buffer has no space
+     * <p>
+     * 4. Give ID, data type and data(no header) to write
      */
     private void reading() {
         while (true) {
             ByteBuffer buffer = ByteBuffer.allocate(6);
+            buffer.order(ByteOrder.BIG_ENDIAN);
             try {
                 client.read(buffer);
             } catch (IOException e) {
@@ -59,9 +97,17 @@ public class ClientInfo {
             }
             buffer.flip();
 
-            char type = buffer.getChar();
-            int size = buffer.getInt();
+            char type = 'I';
+            int size = 0;
+            try {
+                type = buffer.getChar();
+                size = buffer.getInt();
+            } catch (BufferUnderflowException e1) {
+                break;
+            }
+
             buffer = ByteBuffer.allocate(size);
+            buffer.order(ByteOrder.BIG_ENDIAN);
 
             while (buffer.hasRemaining()) {
                 try {
@@ -72,7 +118,10 @@ public class ClientInfo {
             }
 
             buffer.flip();
-            writeToAll.write(ID, type, buffer);
+            ReceivedData receivedData = new ReceivedData(this, DataType.valueOf(type), buffer);
+            processing.getProcessMap().get(DataType.valueOf(type)).accept(receivedData, callMap.getMap());
         }
+        disconnectCallBack.disconnect(this);
     }
+
 }
