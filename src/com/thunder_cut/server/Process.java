dcd1a9b38d4adde5b10558cffa7dfa0a76effef7
Arakene <kbh97102@naver.com>
2020-02-22 11:28:19 +0900

    Update Process

--- src/com/thunder_cut/server/Process.java
+++ src/com/thunder_cut/server/Process.java
@@ -13,8 +13,9 @@ import com.thunder_cut.server.data.SendingData;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
-import java.util.*;
-import java.util.function.BiConsumer;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
 import java.util.function.Consumer;
 
 /**
@@ -49,40 +50,39 @@ public class Process {
         try {
             commandProcess.getCommandMap().get(CommandType.acceptable(commandToken[0], data.getSrc().isOp())).accept(data, commandToken);
         } catch (NullPointerException e) {
-            String errorMessage = "Error! Given command do not exist";
-            SendingData error = new SendingData(data.getSrc(), data.getSrc(), DataType.MSG, errorMessage.getBytes());
-            write(data.getSrc(), error);
+            errorMessage(data.getSrc(), clientMap);
+        }
+    }
+
+    private void errorMessage(ClientInformation src, Map<ClientInformation, List<ClientInformation>> clientMap) {
+        String errorMessage = "Error! Given command do not exist";
+        int srcIndex = 0;
+        for (int destIndex = 0; destIndex < clientMap.get(src).size(); destIndex++) {
+            ClientInformation destination = clientMap.get(src).get(destIndex);
+            if (src == destination) {
+                srcIndex = destIndex;
+                SendingData sendingData = new SendingData(srcIndex, destIndex, DataType.MSG, errorMessage.getBytes());
+                write(src, destination, sendingData);
+                return;
+            }
         }
     }
 
     /**
-     * Generate message data and write to all client in client list
+     * Generate message or image data and write to all client in client list
      *
      * @param data      this have data, type, src
      * @param clientMap client list
      */
-    private void message(ReceivedData data, Map<ClientInformation, List<ClientInformation>> clientMap) {
-        for (ClientInformation dest : clientMap.get(data.getSrc())) {
-            SendingData sendingData = new SendingData(data.getSrc(), dest, data.getDataType(), data.getBuffer().array());
-            write(dest, sendingData);
-        }
-        for(int i=0;i<clientMap.get(data.getSrc()).size();i++){
-            ClientInformation destination = clientMap.get(data.getSrc()).get(i);
-            SendingData sendingData = new SendingData(data.getSrc(), destination, data.getDataType(), data.getBuffer().array());
-            write(destination, sendingData);
-        }
-    }
-
-    /**
-     * Generate image data and write to all client in client list
-     *
-     * @param data      this have data, type, src
-     * @param clientMap client list
-     */
-    private void image(ReceivedData data, Map<ClientInformation, List<ClientInformation>> clientMap) {
-        for (ClientInformation dest : clientMap.get(data.getSrc())) {
-            SendingData sendingData = new SendingData(data.getSrc(), dest, data.getDataType(), data.getBuffer().array());
-            write(dest, sendingData);
+    private void notCommand(ReceivedData data, Map<ClientInformation, List<ClientInformation>> clientMap) {
+        int srcIndex = 0;
+        for (int index = 0; index < clientMap.get(data.getSrc()).size(); index++) {
+            ClientInformation destination = clientMap.get(data.getSrc()).get(index);
+            if (data.getSrc() == destination) {
+                srcIndex = index;
+            }
+            SendingData sendingData = new SendingData(srcIndex, index, data.getDataType(), data.getBuffer().array());
+            write(data.getSrc(), destination, sendingData);
         }
     }
 
@@ -92,23 +92,19 @@ public class Process {
      * @param dest client who received data
      * @param data data for write
      */
-    private synchronized void write(ClientInformation dest, SendingData data) {
+    private synchronized void write(ClientInformation src,ClientInformation dest, SendingData data) {
         try {
-            dest.getClient().write(data.generateDataByType());
+            dest.getClient().write(data.generateDataByType(src));
         } catch (IOException | NullPointerException e) {
             disconnect.accept(dest);
         }
     }
 
-    public void processWithType(ReceivedData data, Map<ClientInformation, List<ClientInformation>> clientMap){
-        if(data.getDataType().equals(DataType.MSG)){
-            message(data, clientMap);
-        }
-        else if(data.getDataType().equals(DataType.IMG)){
-            image(data, clientMap);
-        }
-        else{
+    public void processWithType(ReceivedData data, Map<ClientInformation, List<ClientInformation>> clientMap) {
+        if (data.getDataType().equals(DataType.CMD)) {
             command(data, clientMap);
+        } else {
+            notCommand(data, clientMap);
         }
     }
 }
